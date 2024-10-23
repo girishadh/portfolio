4656bb711435893e99b76b34d92bdde5a7fd0dee
Girish Adhikari <adhgirish@gmail.com>
2024-10-23 13:50:20 +0545

    superuser

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7n&3#x0^-x+=fyql!s*+x)jakl095xbuaf5+kp5w-6@q0mz7pi'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['.vercel.app']
 
 
